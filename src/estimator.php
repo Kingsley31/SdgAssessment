179c2677aa6dc4880777f7759b20bbb3a13ba5d9
Kingsley31 <kingsleyemeka31@gmail.com>
2020-04-14 18:01:28 +0100

    Completed Challenge 3

--- src/estimator.php
+++ src/estimator.php
@@ -37,8 +37,34 @@ function getHospitalBedsByRequestedTime($totalHospitalBeds,$severeCasesByRequest
      return intval(($totalHospitalBeds * (35/100)) - $severeCasesByRequestedTime);
 } 
 
+function getCasesForICUForICUByRequestedTime($infectionsByRequestedTime){
+   return $infectionsByRequestedTime * (5/100);
+}
 
-function getImpact($reportedCases,$duration,$periodType,$totalHospitalBeds){
+
+function getCasesForVentilatorsByRequestedTime($infectionsByRequestedTime){
+  return $infectionsByRequestedTime * (2/100);
+}
+
+function getNumOfDays($duration,$periodType){
+    $num_of_days=0;
+    if($periodType=="days"){
+      $num_of_days=$duration;
+    }else if($periodType=="weeks"){
+      $num_of_days= $duration * 7;
+    }else if($periodType=="months"){
+      $num_of_days= $duration * 30;
+    }
+    return $num_of_days;
+}
+
+function getDollarsInFlight($infectionsByRequestedTime,$duration,$periodType,$avgDailyIncomeInUSD,$avgDailyIncomePopulation){
+    $num_of_days=getNumOfDays($duration,$periodType);
+    return intdiv(($infectionsByRequestedTime * $avgDailyIncomePopulation * $avgDailyIncomeInUSD), $num_of_days);
+}
+
+
+function getImpact($reportedCases,$duration,$periodType,$totalHospitalBeds,$avgDailyIncomeInUSD,$avgDailyIncomePopulation){
   $impact=[];
 
   $currentlyInfected=getCurrentlyInfected($reportedCases);
@@ -53,10 +79,19 @@ function getImpact($reportedCases,$duration,$periodType,$totalHospitalBeds){
   $hospitalBedsByRequestedTime=getHospitalBedsByRequestedTime($totalHospitalBeds,$severeCasesByRequestedTime);
   $impact["hospitalBedsByRequestedTime"]=$hospitalBedsByRequestedTime;
 
+  $casesForICUByRequestedTime=getCasesForICUForICUByRequestedTime($infectionsByRequestedTime);
+  $impact["casesForICUByRequestedTime"]=$casesForICUByRequestedTime;
+
+  $casesForVentilatorsByRequestedTime=getCasesForVentilatorsByRequestedTime($infectionsByRequestedTime);
+  $impact["casesForVentilatorsByRequestedTime"]=$casesForVentilatorsByRequestedTime;
+
+  $dollarsInFlight=getDollarsInFlight($infectionsByRequestedTime,$duration,$periodType,$avgDailyIncomeInUSD,$avgDailyIncomePopulation);
+  $impact["dollarsInFlight"]=$dollarsInFlight;
+
   return $impact;
 }
 
-function getSeverImpact($reportedCases,$duration,$periodType,$totalHospitalBeds){
+function getSeverImpact($reportedCases,$duration,$periodType,$totalHospitalBeds,$avgDailyIncomeInUSD,$avgDailyIncomePopulation){
   $severeImpact=[];
   $currentlyInfected_severe=getCurrentlyInfectedForSeverImpact($reportedCases);
   $severeImpact["currentlyInfected"]=$currentlyInfected_severe;
@@ -70,6 +105,15 @@ function getSeverImpact($reportedCases,$duration,$periodType,$totalHospitalBeds)
   $hospitalBedsByRequestedTime=getHospitalBedsByRequestedTime($totalHospitalBeds,$severeCasesByRequestedTime_severe);
   $severeImpact["hospitalBedsByRequestedTime"]=$hospitalBedsByRequestedTime;
 
+  $casesForICUByRequestedTime=getCasesForICUForICUByRequestedTime($infectionsByRequestedTime_severe);
+  $severeImpact["casesForICUByRequestedTime"]=$casesForICUByRequestedTime;
+
+  $casesForVentilatorsByRequestedTime=getCasesForVentilatorsByRequestedTime($infectionsByRequestedTime_severe);
+  $severeImpact["casesForVentilatorsByRequestedTime"]=$casesForVentilatorsByRequestedTime;
+
+  $dollarsInFlight=getDollarsInFlight($infectionsByRequestedTime_severe,$duration,$periodType,$avgDailyIncomeInUSD,$avgDailyIncomePopulation);
+  $severeImpact["dollarsInFlight"]=$dollarsInFlight;
+
   return $severeImpact;
 }
 
@@ -82,11 +126,13 @@ function covid19ImpactEstimator($data)
   $duration=$data["timeToElapse"];
   $periodType=$data["periodType"];
   $totalHospitalBeds=$data["totalHospitalBeds"];
+  $avgDailyIncomeInUSD=$data["region"]["avgDailyIncomeInUSD"];
+  $avgDailyIncomePopulation=$data["region"]["avgDailyIncomePopulation"];
 
  
   $output["data"] =$data;
-  $output["impact"]=getImpact($reportedCases,$duration,$periodType,$totalHospitalBeds);
-  $output["severeImpact"]=getSeverImpact($reportedCases,$duration,$periodType,$totalHospitalBeds);
+  $output["impact"]=getImpact($reportedCases,$duration,$periodType,$totalHospitalBeds,$avgDailyIncomeInUSD,$avgDailyIncomePopulation);
+  $output["severeImpact"]=getSeverImpact($reportedCases,$duration,$periodType,$totalHospitalBeds,$avgDailyIncomeInUSD,$avgDailyIncomePopulation);
   $data=$output;
   
   return $data;
